56946c191e744b9fa28a35af74653b8b9aef22fb
yan <yan@brave.com>
2019-10-03 18:11:48 -0700

    add buffering

--- workers/micProcessor.js
+++ workers/micProcessor.js
@@ -1,6 +1,30 @@
+const gate = 0.03 // TODO: make this adjustable
+const bufSize = 10
+
+/**
+ * Create a circular buffer of length n
+ */
+function CircularBuffer (n) {
+  this.max_length = n
+  this.array = []
+}
+CircularBuffer.prototype.add = function (item) {
+  // adds an element to the buffer
+  this.array.push(item)
+  if (this.array.length > this.max_length) {
+    // remove first item of array
+    this.array.shift()
+  }
+}
+CircularBuffer.prototype.average = function () {
+  // returns average of values in the buffer
+  return this.array.reduce((a, b) => a + b, 0) / this.array.length
+}
+
 class MicProcessor extends AudioWorkletProcessor {
   process (inputs, outputs, parameters) {
     const channels = inputs[0]
+    this.volumeBuffer = new CircularBuffer(bufSize)
     this.volumeAudioProcess(channels)
     return true
   }
@@ -9,14 +33,13 @@ class MicProcessor extends AudioWorkletProcessor {
     // assume stereo
     const buf1 = channels[0]
     const buf2 = channels[1]
-    const prevVolume = volume
-    volume = rms(buf1, buf2)
-    if (volume > gate && prevVolume > gate) {
-      // part of a contiguous chunk
+    const volume = rms(buf1, buf2)
+    this.volumeBuffer.add(volume)
+    if (this.volumeBuffer.average() > gate) {
       chunk.channel1.push(buf1)
       chunk.channel2.push(buf2)
     } else {
-      if (chunk.channel1.length || chunk.channel2.length) {
+      if (chunk.channel1.length > bufSize) {
         this.port.postMessage(chunk)
       }
       chunk.channel1 = []
@@ -25,12 +48,10 @@ class MicProcessor extends AudioWorkletProcessor {
   }
 }
 
-const gate = 0.05 // TODO: make this adjustable
 const chunk = {
   channel1: [],
   channel2: []
 }
-let volume = 0
 
 function rms (buf1, buf2) {
   const sum = squareSum(buf1) + squareSum(buf2)
